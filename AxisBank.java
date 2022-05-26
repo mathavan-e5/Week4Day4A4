d153e41829b074a32599f8344798273e4d06bd37
Mathavan M <104998416+volthatter@users.noreply.github.com>
2022-05-26 20:13:57 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,20 @@
+package Week4Day2;
+
+public class AxisBank extends BankInfo {
+	
+	
+	public void deposit() {
+		System.out.println("100000");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		AxisBank call=new AxisBank();
+		call.deposit();
+		call.Saving();
+		call.Fixed();
+		
+	}
+
+}
---
+++ BankInfo.java
@@ -0,0 +1,20 @@
+package Week4Day2;
+
+public class BankInfo {
+	public void Saving() {
+		System.out.println("5000");
+		
+	}
+	public void Fixed() {
+		System.out.println("500000");
+		
+		
+	}
+	public void deposit() {
+		System.out.println("100000");
+		
+	}
+	
+	
+
+}
